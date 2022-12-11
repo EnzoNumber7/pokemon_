ead9ef6ee6abfa041c78825ab89fdfefdd790331
EnzoNumber7 <ephilippe@gaming.tech>
2022-12-11 16:42:38 +0100

    new

--- pokemon/myapp/src/pages/home.js
+++ pokemon/myapp/src/pages/home.js
@@ -1,9 +1,33 @@
 import Menu from "../components/menu";
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Home(props){
-    return <body>
+    return <div>
         <Menu />
-    </body>
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Liste des Pokemon</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Liste de vos Pokemon</Card.Title>
+                            </Card.Body>
+                        </Card>  
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    </div>
 }
 
 export default Home;
