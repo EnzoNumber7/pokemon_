1b90f56b68c73b347e9d73dbe5cf6d40f5b1b70b
EnzoNumber7 <117282665+EnzoNumber7@users.noreply.github.com>
2022-12-14 19:38:02 +0100

    Update footer.js

--- pokemon/myapp/src/components/footer.js
+++ pokemon/myapp/src/components/footer.js
@@ -1,9 +1,25 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import logo from '../img/pokemonLogoFooter.png';
 
 function Footer(){
-    return <div>
-        
+    return <div className='bgColorBlue footer ' sticky="botom">
+        <Container>
+            <Row>
+                <Col xs={12} lg={5} className=' text-center'>
+                    <img src={logo} />
+                    <p className="footer-title">Nous Contacter</p>
+                    <p className="footer-text">Email: ephilippe@gaming.tech / vfovet@gaming.tech</p>
+                </Col>
+                <Col xs={12} lg={5} className='text-center'>
+                    <p className="footer-title margin-top">Pour plus d'information sur Pokemon</p>
+                    <a className="footer-text" href="https://www.pokemon.com/fr">Le site Web officiel Pokémon</a>
+                </Col>
+            </Row>
+        </Container>
     </div>
+
 }
+
+export default Footer;
