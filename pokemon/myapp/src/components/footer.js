e040bd29c159cdbc40d5e948c89249ad87330458
EnzoNumber7 <117282665+EnzoNumber7@users.noreply.github.com>
2022-12-14 17:36:54 +0100

    Create footer.js

---
+++ pokemon/myapp/src/components/footer.js
@@ -0,0 +1,9 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Footer(){
+    return <div>
+        
+    </div>
+}
